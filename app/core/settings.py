de024200f449b912993375c1dad0eabaac3a5901
Adilet <eraliev.adilet006@gmail.com>
2025-01-19 22:23:20 +0600

    Changed a settigns.py file, changed a DBSettings class structure, and finding a .evn file data

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,15 +1,26 @@
 
+import os
+from pathlib import Path
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
+BASE_DIR = Path(__file__).parent.parent
 
-class DBSettings(BaseSettings):
+
+class DBSettings(BaseModel):
     DB_URL: str
 
 class Settings(BaseSettings):
-    db_url: DBSettings = DBSettings()
+    db: DBSettings = DBSettings()
     api_prefix: str = '/api/v1'
     PORT: int = 8000
     HOST: str = 'localhost'
 
+    class Config:
+        env_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+
 
 settings = Settings()
+
+if __name__ == "__main__":
+    print(settings.db.DB_URL)
