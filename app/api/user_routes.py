a70059d031873cf87993c6816346679293dbfda1
Adilet <eraliev.adilet006@gmail.com>
2025-01-26 19:07:15 +0600

    Created all crud routes for user

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
 
-from app.core import db_helper
-from app.repositories.user_repository import UserRepository
+from fastapi import APIRouter, Depends
+
+from app.api.utils import get_user_service, get_user_by_id
+from app.models import UserModel
 from app.schemas import UserCreateSchema, UserUpdateSchema
 from app.services.user_services import UserService
 
@@ -11,28 +12,33 @@ router = APIRouter(
     prefix='/users'
 )
 
-def get_user_service(
-        session: AsyncSession = Depends(db_helper.session_getter)
-) -> UserService:
-    repository = UserRepository(session)
-    return UserService(repository)
+UserServiceDepends = Annotated[UserService, Depends(get_user_service)]
+UserByIdDepends = Annotated[UserModel, Depends(get_user_by_id)]
 
 @router.post('/')
-async def create(user: UserCreateSchema, user_service: UserService = Depends(get_user_service)):
+async def create(user: UserCreateSchema, user_service: UserServiceDepends):
     return await user_service.create(user)
 
 
 @router.get('/')
-async def get_all_users(user_service: UserService = Depends(get_user_service)):
+async def get_users(user_service: UserServiceDepends):
     return await user_service.get_all()
 
 @router.get('/{user_id}')
-async def get_user_by_id(user_id: int, user_service: UserService = Depends(get_user_service)):
+async def get_user(user_id: int, user_service: UserServiceDepends):
     return await user_service.get_by_id(user_id)
 
-# @router.put('/{user_id}')
-# async def update_user(
-#         user_id: int, user_update: UserUpdateSchema,
-#         user_service: UserService = Depends(get_user_service)
-#                       ):
-#     return await user_service.update()
+@router.put('/{user_id}')
+async def update_user(
+        user_update: UserUpdateSchema,
+        user_service: UserServiceDepends,
+        user: UserByIdDepends
+):
+    return await user_service.update(user, user_update)
+
+@router.delete('/{user_id}')
+async def delete_user(
+        user_service: UserServiceDepends,
+        user: UserByIdDepends
+):
+    return await user_service.delete(user)
---
+++ app/api/utils/user_dependencies.py
@@ -0,0 +1,13 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core import db_helper
+from app.repositories import UserRepository
+from app.services.user_services import UserService
+
+
+def get_user_service(
+        session: AsyncSession = Depends(db_helper.session_getter)
+) -> UserService:
+    repository = UserRepository(session)
+    return UserService(repository)
