19a784fb76bafd6050b26d57a329dda22f270f5a
Adilet <eraliev.adilet006@gmail.com>
2025-01-26 19:08:08 +0600

    Added user_dependencies utils to help and to follow DRY

--- app/api/utils/user_dependencies.py
+++ app/api/utils/user_dependencies.py
@@ -2,6 +2,7 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core import db_helper
+from app.models import UserModel
 from app.repositories import UserRepository
 from app.services.user_services import UserService
 
@@ -11,3 +12,9 @@ def get_user_service(
 ) -> UserService:
     repository = UserRepository(session)
     return UserService(repository)
+
+async def get_user_by_id(
+        user_id: int,
+        user_service: UserService = Depends(get_user_service)
+) -> UserModel:
+    return await user_service.get_by_id(user_id)
