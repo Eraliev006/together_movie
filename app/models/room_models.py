c22a080576669c2d8b707520a84ff02ced531da3
Adilet <eraliev.adilet006@gmail.com>
2025-02-01 20:45:38 +0600

    Modified a RoomModel fixed slug field: mapped_column(str, ...) > mapped_column(...)

--- app/models/room_models.py
+++ app/models/room_models.py
@@ -10,7 +10,7 @@ class RoomModel(Base):
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
     name: Mapped[str] = mapped_column(String(50), unique=True)
     host_id: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
-    slug: Mapped[str] = mapped_column(str, nullable=False, unique=True)
+    slug: Mapped[str] = mapped_column(nullable=False, unique=True)
 
     users: Mapped[list['UserModel']] = relationship( back_populates='room', foreign_keys=[UserModel.current_room_id])
     host: Mapped['UserModel'] = relationship(foreign_keys=[host_id])
