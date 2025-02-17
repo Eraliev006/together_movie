a764fd47632e424b59c93be084249ecba81f2e6c
Adilet <eraliev.adilet006@gmail.com>
2025-02-17 07:36:04 +0600

    Added: added actor crud routes

--- app/api/utils/__init__.py
+++ app/api/utils/__init__.py
@@ -1,3 +1,4 @@
 from .user_dependencies import get_user_service,get_user_by_id
 from .room_dependencies import get_room_service, get_room_by_slug
 from .genre_dependencies import get_genre_service, get_genre_by_slug
+from .actor_dependencies import get_actor_by_id, get_actor_service
