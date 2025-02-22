5d3109f999ecc32433f4b69039341358810fb577
Adilet <eraliev.adilet006@gmail.com>
2025-02-23 00:11:14 +0600

    Register a custom exception handlers

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ from fastapi import FastAPI
 
 from app.api import router
 from app.core import settings, db_helper
-
+from app.exceptions import UserNotFoundException, user_not_found_exception_handler, user_already_exists_handler, \
+    UserAlreadyExistsException
 
 
 @asynccontextmanager
@@ -17,6 +18,8 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.add_exception_handler(UserNotFoundException, user_not_found_exception_handler)
+app.add_exception_handler(UserAlreadyExistsException, user_already_exists_handler)
 app.include_router(router)
 
 if __name__ == '__main__':
