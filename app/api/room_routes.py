6289213540729b6014552eb02d51fcbce90de5a9
Adilet <eraliev.adilet006@gmail.com>
2025-03-01 14:50:13 +0600

    Refactor: deleted unusefull function and refactor delete function

--- app/api/room_routes.py
+++ app/api/room_routes.py
@@ -2,10 +2,10 @@ from typing import Annotated
 
 from fastapi import APIRouter
 from fastapi.params import Depends
+from starlette.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_204_NO_CONTENT
 
-from app.api.utils import get_room_service, get_room_by_slug
-from app.models import RoomModel
-from app.schemas import RoomCreateSchema
+from app.api.utils import get_room_service
+from app.schemas import RoomCreateSchema, RoomOutSchema
 from app.services.room_services import RoomServices
 
 router = APIRouter(
@@ -14,26 +14,25 @@ router = APIRouter(
 )
 
 RoomServiceDepends = Annotated[RoomServices, Depends(get_room_service)]
-RoomByIdDepends = Annotated[RoomModel, Depends(get_room_by_slug)]
 
-@router.post('/')
+@router.post('/', status_code=HTTP_201_CREATED, response_model=RoomOutSchema)
 async def create_room(
         room: RoomCreateSchema,
         room_service: RoomServiceDepends
 ):
     return await room_service.create(room)
 
-@router.get('/')
+@router.get('/', status_code=HTTP_200_OK, response_model=list[RoomOutSchema])
 async def get_all_room(room_service: RoomServiceDepends):
     return await room_service.get_all()
 
-@router.get('/{room_slug}')
+@router.get('/{room_slug}', status_code=HTTP_200_OK, response_model=RoomOutSchema)
 async def get_one(room_slug:str, room_service: RoomServiceDepends):
     return await room_service.get_one(room_slug)
 
-@router.delete('/{room_slug}')
+@router.delete('/{room_slug}', status_code=HTTP_204_NO_CONTENT, response_model = None)
 async def delete(
         room_service: RoomServiceDepends,
-        room: RoomByIdDepends
+        room_slug: str
 ):
-    return await room_service.delete(room)
+    return await room_service.delete(room_slug)
